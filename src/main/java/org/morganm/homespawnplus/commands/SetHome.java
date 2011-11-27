89d33aa836bb627c37e8afc3bc89c56f05ec5568
Mark Morgan <morganm.qc@gmail.com>
2011-11-27 14:28:11 -0800

    superperms cleanup for plugin.yml

--- src/main/java/org/morganm/homespawnplus/commands/SetHome.java
+++ src/main/java/org/morganm/homespawnplus/commands/SetHome.java
@@ -5,6 +5,7 @@ package org.morganm.homespawnplus.commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
+import org.morganm.homespawnplus.HomeSpawnPlus;
 import org.morganm.homespawnplus.command.BaseCommand;
 import org.morganm.homespawnplus.storage.Storage;
 
@@ -15,7 +16,7 @@ import org.morganm.homespawnplus.storage.Storage;
  */
 public class SetHome extends BaseCommand
 {
-//	private static final String OTHER_SETHOME_PERMISSION = HomeSpawnPlus.BASE_PERMISSION_NODE + "command.sethome.others";
+	private static final String SETHOME_NAMED_PERMISSION = HomeSpawnPlus.BASE_PERMISSION_NODE + "command.sethome.named";
 	
 	@Override
 	public String[] getCommandAliases() { return new String[] {"homeset"}; }
@@ -30,12 +31,16 @@ public class SetHome extends BaseCommand
 			return true;
 		
 		if( args.length > 0 ) {
-			if( !args[0].equals(Storage.HSP_BED_RESERVED_NAME) && !args[0].endsWith("_" + Storage.HSP_BED_RESERVED_NAME )) {
-				if( util.setNamedHome(p.getName(), p.getLocation(), args[0], p.getName()) )
-					util.sendMessage(p, "Home \""+args[0]+"\" set successfully.");
+			if( plugin.hasPermission(p, SETHOME_NAMED_PERMISSION) ) {
+				if( !args[0].equals(Storage.HSP_BED_RESERVED_NAME) && !args[0].endsWith("_" + Storage.HSP_BED_RESERVED_NAME )) {
+					if( util.setNamedHome(p.getName(), p.getLocation(), args[0], p.getName()) )
+						util.sendMessage(p, "Home \""+args[0]+"\" set successfully.");
+				}
+				else
+					util.sendMessage(p, "Cannot used reserved name "+args[0]);
 			}
 			else
-				util.sendMessage(p, "Cannot used reserved name "+args[0]);
+				util.sendMessage(p, "You do not have permission to set named homes");
 		}
 		else {
 			if( util.setHome(p.getName(), p.getLocation(), p.getName(), true, false) )

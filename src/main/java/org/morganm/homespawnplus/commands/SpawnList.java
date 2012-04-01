b4c44d33115aa1f53b056c3521c5d4094f6f133d
Mark Morgan <morganm.qc@gmail.com>
2012-03-31 23:24:41 -0700

    added "-m" flag to spawnlist to view Minecraft map spawn as well

--- src/main/java/org/morganm/homespawnplus/commands/SpawnList.java
+++ src/main/java/org/morganm/homespawnplus/commands/SpawnList.java
@@ -3,9 +3,13 @@
  */
 package org.morganm.homespawnplus.commands;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
@@ -36,10 +40,23 @@ public class SpawnList extends BaseCommand {
 		return executePrivate(p, command, args);
 	}
 	
-	private boolean executePrivate(CommandSender p, Command command, String[] args) {
+	private boolean executePrivate(CommandSender p, Command command, String[] originalArgs) {
+		boolean showMapSpawn = false;
+		List<String> args = new ArrayList<String>(originalArgs.length);
+		
 		String world = "all";
-		if( args.length > 0 )
-			world = args[0];
+		if( originalArgs.length > 0 ) {
+			for(int i=0; i < originalArgs.length; i++) {
+				if( originalArgs[i].equals("-m") ) {
+					showMapSpawn = true;
+				}
+				else
+					args.add(originalArgs[i]);
+			}
+			
+			if( args.size() > 0 )
+				world = args.get(0);
+		}
 		
 		final Set<org.morganm.homespawnplus.entity.Spawn> spawns = plugin.getStorage().getSpawnDAO().findAllSpawns();
 		
@@ -54,6 +71,27 @@ public class SpawnList extends BaseCommand {
 				util.sendLocalizedMessage(p, HSPMessages.CMD_SPAWNLIST_FOR_WORLD,
 						"world", world);
 //				util.sendMessage(p, "Spawn list on world \""+world+"\": ");
+			
+			if( showMapSpawn ) {
+				List<World> worlds = null;
+				if( world.equals("all") ) {
+					worlds = plugin.getServer().getWorlds();
+				}
+				else {
+					worlds = new ArrayList<World>();
+					World w = plugin.getServer().getWorld(world);
+					if( w != null )
+						worlds.add(w);
+				}
+				
+				for(World w : worlds) {
+					Location l = w.getSpawnLocation();
+					p.sendMessage(util.getDefaultColor() + "id: " + ChatColor.RED + "none " + util.getDefaultColor()
+							+ util.shortLocationString(l)
+							+ ChatColor.GREEN + " (map spawn)"
+							);
+				}
+			}
 			
 			for(org.morganm.homespawnplus.entity.Spawn spawn : spawns) {
 				if( !world.equals("all") && !world.equals(spawn.getWorld()) )

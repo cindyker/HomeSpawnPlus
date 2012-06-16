7c99828c54dc31fd53718dc7bc43ab4249213378
Mark Morgan <morganm.qc@gmail.com>
2012-06-16 16:41:14 -0700

    fix for when admin attempts to use WG strategy but WG is not installed

--- src/main/java/org/morganm/homespawnplus/strategies/SpawnWorldGuardRegion.java
+++ src/main/java/org/morganm/homespawnplus/strategies/SpawnWorldGuardRegion.java
@@ -17,8 +17,15 @@ public class SpawnWorldGuardRegion extends BaseStrategy {
 
 	@Override
 	public StrategyResult evaluate(StrategyContext context) {
-		if( wgInterface == null )
-			wgInterface = new WorldGuardInterface(plugin);
+		if( wgInterface == null ) {
+			if( plugin.getServer().getPluginManager().getPlugin("WorldGuard") != null ) { 
+				wgInterface = new WorldGuardInterface(plugin);
+			}
+			else {
+				log.warning("Attempted to use "+getStrategyConfigName()+" without WorldGuard installed. Strategy ignored.");
+				return null;
+			}
+		}
 		
 		return new StrategyResult( wgInterface.getWorldGuardSpawnLocation(context.getPlayer()) );
 	}

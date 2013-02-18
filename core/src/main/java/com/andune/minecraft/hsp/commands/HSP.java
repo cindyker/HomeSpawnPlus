db81ad572684e9b9f993fcbaa798379f2f080d1f
Andune <andune.alleria@gmail.com>
2013-02-18 13:35:27 -0800

    manual merge /hsp dedup and /hsp lowercase features

--- core/src/main/java/com/andune/minecraft/hsp/commands/HSP.java
+++ core/src/main/java/com/andune/minecraft/hsp/commands/HSP.java
@@ -30,6 +30,10 @@
  */
 package com.andune.minecraft.hsp.commands;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.inject.Inject;
 
 import com.andune.minecraft.hsp.HSPMessages;
@@ -41,6 +45,9 @@ import com.andune.minecraft.hsp.integration.multiverse.MultiversePortals;
 import com.andune.minecraft.hsp.integration.worldborder.WorldBorder;
 import com.andune.minecraft.hsp.integration.worldguard.WorldGuard;
 import com.andune.minecraft.hsp.server.api.CommandSender;
+import com.andune.minecraft.hsp.server.api.Scheduler;
+import com.andune.minecraft.hsp.storage.StorageException;
+import com.andune.minecraft.hsp.storage.dao.HomeDAO;
 import com.andune.minecraft.hsp.util.BackupUtil;
 
 /**
@@ -55,6 +62,7 @@ public class HSP extends BaseCommand {
     @Inject WorldBorder worldBorder;
     @Inject WorldGuard worldGuard;
     @Inject BackupUtil backupUtil;
+    @Inject Scheduler scheduler;
     
 	@Override
 	public String getUsage() {
@@ -107,6 +115,16 @@ public class HSP extends BaseCommand {
                     + (worldGuard.isEnabled() ? "enabled" : "disabled")
                     + ", detected version " + worldGuard.getVersion());
         }
+        // admin command to clean up any playerName-case-caused dups
+        else if( args[0].startsWith("dedup") || args[0].equals("dd") ) {
+            sender.sendMessage("Starting async HSP database home playerName dup cleanup");
+            scheduler.scheduleAsyncDelayedTask(new DeDupDatabaseRunner(sender), 0);
+        }
+        // admin command to switch all database player names to lowercase
+        else if( args[0].startsWith("lowercase") || args[0].equals("lc") ) {
+            sender.sendMessage("Starting async HSP database playerName-to-lowercase conversion");
+            scheduler.scheduleAsyncDelayedTask(new LowerCaseDatabaseRunner(sender), 0);
+        }
 		else if( args[0].startsWith("backup") ) {
 		    String errorMessage = backupUtil.backup();
 		    if( errorMessage == null ) {
@@ -139,7 +157,138 @@ public class HSP extends BaseCommand {
 
 		return true;
 	}
-	
+
+    private class DeDupDatabaseRunner implements Runnable {
+        private CommandSender sender;
+        
+        public DeDupDatabaseRunner(CommandSender sender) {
+            this.sender = sender;
+        }
+        
+        public void run() {
+            log.debug("DeDupDatabaseRunner running");
+            int dupsCleaned=0;
+            try {
+                storage.setDeferredWrites(true);
+                final HomeDAO homeDAO = storage.getHomeDAO();
+
+                // we fix dups by player, so we can keep the most recent home in the
+                // event of duplicates. So we keep track of player homes we have fixed
+                // so we can skip any others we come across as we're iterating the
+                // allHomes hash.
+                HashSet<String> playersFixed = new HashSet<String>(100);
+
+                Set<? extends com.andune.minecraft.hsp.entity.Home> allHomes = homeDAO.findAllHomes();
+                for(com.andune.minecraft.hsp.entity.Home home : allHomes) {
+                    final String lcPlayerName = home.getPlayerName().toLowerCase();
+                    if( playersFixed.contains(lcPlayerName) )
+                        continue;
+                    log.debug("home check for home name {}",lcPlayerName);
+
+                    final HashMap<String, com.andune.minecraft.hsp.entity.Home> dupCheck = new HashMap<String, com.andune.minecraft.hsp.entity.Home>();
+                    Set<? extends com.andune.minecraft.hsp.entity.Home> playerHomes = homeDAO.findHomesByPlayer(lcPlayerName);
+                    // look for duplicates and delete all but the newest if we find any
+                    for(com.andune.minecraft.hsp.entity.Home playerHome : playerHomes) {
+                        final String homeName = playerHome.getName();
+                        log.debug("dup check for home name \"{}\"", homeName);
+                        // ignore no-name homes, they don't have to be unique
+                        if( homeName == null )
+                            continue;
+
+                        // have we seen this home before?
+                        com.andune.minecraft.hsp.entity.Home dup = dupCheck.get(homeName);
+                        if( dup != null ) {
+                            log.debug("found dup for home {}",homeName);
+                            // determine which one is oldest and delete the oldest one
+                            if( dup.getLastModified().getTime() < playerHome.getLastModified().getTime() ) {
+                                // dup is oldest, delete it
+                                log.info("Deleting oldest duplicate home (id "+dup.getId()+", name "+dup.getName()+") for player "+lcPlayerName);
+                                homeDAO.deleteHome(dup);
+                                dupCheck.put(homeName, playerHome); // record new record in our dup hash
+                            }
+                            else {
+                                // playerHome is oldest, delete it
+                                log.info("Deleting oldest duplicate home (id "+playerHome.getId()+", name "+playerHome.getName()+") for player "+lcPlayerName);
+                                homeDAO.deleteHome(playerHome);
+                            }
+                            dupsCleaned++;
+                        }
+                        else {
+                            log.debug("no dup found for home {}",homeName);
+                            dupCheck.put(homeName, playerHome); // we have now, record it
+                        }
+                    }
+
+                    playersFixed.add(lcPlayerName);
+                }
+
+                storage.flushAll();
+            }
+            catch(StorageException e) {
+                log.error("Caught exception processing /hsp dedup", e);
+            }
+            finally {
+                storage.setDeferredWrites(false);
+            }
+
+            sender.sendMessage("Database playerName dups have been cleaned up. "+dupsCleaned+" total dups found and cleaned");
+        }
+    }
+    
+    private class LowerCaseDatabaseRunner implements Runnable {
+        private CommandSender sender;
+        
+        public LowerCaseDatabaseRunner(CommandSender sender) {
+            this.sender = sender;
+        }
+        
+        public void run() {
+            log.debug("LowerCaseDatabaseRunner running");
+            int conversions=0;
+            try {
+                storage.setDeferredWrites(true);
+                final HomeDAO homeDAO = storage.getHomeDAO();
+
+                // we fix names by player, so we can keep the most recent home in the
+                // event of duplicates. So we keep track of player homes we have fixed
+                // so we can skip any others we come across as we're iterating the
+                // allHomes hash.
+                HashSet<String> playersFixed = new HashSet<String>(100);
+
+                Set<? extends com.andune.minecraft.hsp.entity.Home> allHomes = homeDAO.findAllHomes();
+                for(com.andune.minecraft.hsp.entity.Home home : allHomes) {
+                    final String lcPlayerName = home.getPlayerName().toLowerCase();
+                    if( playersFixed.contains(lcPlayerName) )
+                        continue;
+                    log.debug("home check for home name {}",lcPlayerName);
+
+                    final Set<? extends com.andune.minecraft.hsp.entity.Home> playerHomes = homeDAO.findHomesByPlayer(lcPlayerName);
+                    for(com.andune.minecraft.hsp.entity.Home playerHome : playerHomes) {
+                        // set home playerName to lower case if it's not already
+                        if( !lcPlayerName.equals(playerHome.getPlayerName()) ) {
+                            log.info("Fixing playerName to lowerCase for home id "+playerHome.getId()+", home name "+playerHome.getName()+" for player "+lcPlayerName);
+                            playerHome.setPlayerName(lcPlayerName);
+                            homeDAO.saveHome(playerHome);
+                            conversions++;
+                        }
+                    }
+
+                    playersFixed.add(lcPlayerName);
+                }
+
+                storage.flushAll();
+            }
+            catch(StorageException e) {
+                log.error("Caught exception processing /hsp lc conversion", e);
+            }
+            finally {
+                storage.setDeferredWrites(false);
+            }
+
+            sender.sendMessage("Database playerNames converted to lowerCase complete. Processed "+conversions+" conversions");
+        }
+    }
+    
 //	private void printUsage(CommandSender p, Command command) {
 //		util.sendMessage(p, command.getUsage());
 //		

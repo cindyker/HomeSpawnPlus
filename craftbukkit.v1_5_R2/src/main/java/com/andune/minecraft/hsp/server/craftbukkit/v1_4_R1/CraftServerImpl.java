720c9e7dcaf89a6c8de6bec1d88a32ea92dd9fe2
Andune <andune.alleria@gmail.com>
2013-04-30 22:46:37 -0700

    add CraftBukkit v1_5_R2 support

---
+++ craftbukkit.v1_5_R2/src/main/java/com/andune/minecraft/hsp/server/craftbukkit/v1_4_R1/CraftServerImpl.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.andune.minecraft.hsp.server.craftbukkit.v1_4_R1;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.craftbukkit.v1_5_R2.CraftServer;
+
+/**
+ * @author andune
+ *
+ */
+public class CraftServerImpl implements com.andune.minecraft.hsp.server.craftbukkit.CraftServer {
+    /* (non-Javadoc)
+     * @see com.andune.minecraft.hsp.server.craftbukkit.CraftServer#registerCommand(org.bukkit.command.Command)
+     */
+    @Override
+    public void registerCommand(Command command) {
+        CraftServer craftServer = (CraftServer) Bukkit.getServer();
+        SimpleCommandMap commandMap = craftServer.getCommandMap();
+        commandMap.register("hsp", command);
+    }
+}

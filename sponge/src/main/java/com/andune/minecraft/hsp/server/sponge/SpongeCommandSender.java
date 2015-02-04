165f1786e00cf370c4038b24f64d0f9c53a83639
Andune <andune.alleria@gmail.com>
2015-02-03 16:40:05 -0800

    Implement Sponge CommandSender for HSP

---
+++ sponge/src/main/java/com/andune/minecraft/hsp/server/sponge/SpongeCommandSender.java
@@ -0,0 +1,58 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2015 Andune (andune.alleria@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+package com.andune.minecraft.hsp.server.sponge;
+
+import com.andune.minecraft.commonlib.i18n.Colors;
+import com.andune.minecraft.hsp.server.api.Server;
+import org.spongepowered.api.util.command.CommandSource;
+
+/**
+ * HSP-specific implementation of CommandSender object for Sponge.
+ *
+ * @author andune
+ */
+public class SpongeCommandSender extends com.andune.minecraft.commonlib.server.sponge.SpongeCommandSender {
+    private final Server server;
+
+    public SpongeCommandSender(CommandSource commandSource, Server server, Colors colors) {
+        super(commandSource, colors);
+        this.server = server;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        super.sendMessage(server.getDefaultColor() + message);
+    }
+
+    @Override
+    public void sendMessage(String[] messages) {
+        if (messages != null && messages.length > 0)
+            messages[0] = server.getDefaultColor() + messages[0];
+        super.sendMessage(messages);
+    }
+}
